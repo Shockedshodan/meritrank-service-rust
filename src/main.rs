592344c9a44e5891ae08a5185648611d19ec8be8
Michael Shestero <shestero@gmail.com>
2023-11-24 20:56:01 +0300

    passing error messages

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use crate::error::GraphManipulationError;
 use crate::graph::{GraphSingleton, NodeId, GRAPH};
 use crate::lib_graph::Weight;
 use nng::{Message, Protocol, Socket};
+
 mod graph; // This module is for graph related operations
-           // #[cfg(feature = "shared")]
-           // mod shared; // This module contains shared data structures
+// #[cfg(feature = "shared")]
+// mod shared; // This module contains shared data structures
 
 mod error;
 mod lib_graph; // This module contains graph related operations and data structures
@@ -21,53 +22,76 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         let req: Message = s.recv()?;
         let slice = req.as_slice();
 
-        let reply: Vec<u8> = if let Ok(((("src", "=", ego), ("dest", "=", target)), ())) =
+        let result: Result<Vec<u8>, Box<dyn std::error::Error>> = if let Ok(((("src", "=", ego), ("dest", "=", target)), ())) =
             rmp_serde::from_slice(slice)
         {
-            // mr_node_score
-            let mut rank = GraphSingleton::get_rank()?;
-            let ego_id: NodeId = GraphSingleton::node_name_to_id(ego)?;
-            let target_id: NodeId = GraphSingleton::node_name_to_id(target)?;
-            let _ = rank.calculate(ego_id, 10)?;
-            let w: Weight = rank.get_node_score(ego_id, target_id)?;
-            let result: Vec<(&str, &str, f64)> = [(ego, target, w)].to_vec();
-            rmp_serde::to_vec(&result)?
-        } else if let Ok(((("src", "=", ego),), ())) = rmp_serde::from_slice(slice) {
-            // mr_scores
-            let mut rank = GraphSingleton::get_rank()?;
-            let node_id: NodeId = GraphSingleton::node_name_to_id(ego)?;
-            let _ = rank.calculate(node_id, 10)?;
-            let result: Vec<(&str, String, Weight)> = rank
-                .get_ranks(node_id, None)?
-                .into_iter()
-                .map(|(n, w)| {
-                    (
-                        ego,
-                        GraphSingleton::node_id_to_name(n).unwrap_or(n.to_string()),
-                        w,
-                    )
-                })
-                .collect();
-            rmp_serde::to_vec(&result)?
-        } else if let Ok((((subject, object, amount),), ())) = rmp_serde::from_slice(slice) {
-            // mr_edge
-            meritrank_add(subject, object, amount)?;
-            let result: Vec<(&str, &str, f64)> = [(subject, object, amount)].to_vec();
-            rmp_serde::to_vec(&result)?
+            mr_node_score(ego, target)
+        } else if let Ok(((("src", "=", ego), ), ())) = rmp_serde::from_slice(slice) {
+            mr_scores(ego)
+        } else if let Ok((((subject, object, amount), ), ())) = rmp_serde::from_slice(slice) {
+            mr_edge(subject, object, amount)
         } else {
-            let err = format!("Error: Cannot understand request {:?}", &req[..]);
+            let err: String = format!("Error: Cannot understand request {:?}", &req[..]);
             println!("{}", err);
-            err.as_bytes().to_vec()
+            Err(err.into())
         };
 
-        let _ = s
-            .send(Message::from(reply.as_slice()))
-            .map_err(|(_, e)| e)?;
+        let reply: Vec<u8> =
+            result
+                .map(|msg| msg )
+                .unwrap_or_else(|e| {
+                    println!("{}", e);
+                    let s: String = e.to_string();
+                    rmp_serde::to_vec(&s).unwrap()
+                });
+
+        let _ = s.send(reply.as_slice()).map_err(|(_, e)| e)?;
     }
     // Ok(())
 }
 
-pub fn meritrank_add(
+fn mr_node_score(ego: &str, target: &str) -> Result<Vec<u8>, Box<dyn std::error::Error + 'static>> {
+    let mut rank = GraphSingleton::get_rank()?;
+    let ego_id: NodeId = GraphSingleton::node_name_to_id(ego)?;
+    let target_id: NodeId = GraphSingleton::node_name_to_id(target)?;
+    let _ = rank.calculate(ego_id, 10)?;
+    let w: Weight = rank.get_node_score(ego_id, target_id)?;
+    let result: Vec<(&str, &str, f64)> = [(ego, target, w)].to_vec();
+    let v: Vec<u8> = rmp_serde::to_vec(&result)?;
+    Ok(v)
+}
+
+fn mr_scores(ego: &str) -> Result<Vec<u8>, Box<dyn std::error::Error + 'static>> {
+    let mut rank = GraphSingleton::get_rank()?;
+    let node_id: NodeId = GraphSingleton::node_name_to_id(ego)?;
+    let _ = rank.calculate(node_id, 10)?;
+    let result: Vec<(&str, String, Weight)> = rank
+        .get_ranks(node_id, None)?
+        .into_iter()
+        .map(|(n, w)| {
+            (
+                ego,
+                GraphSingleton::node_id_to_name(n).unwrap_or(n.to_string()),
+                w,
+            )
+        })
+        .collect();
+    let v: Vec<u8> = rmp_serde::to_vec(&result)?;
+    Ok(v)
+}
+
+fn mr_edge(
+    subject: &str,
+    object: &str,
+    amount: f64,
+) -> Result<Vec<u8>, Box<dyn std::error::Error + 'static>> {
+    meritrank_add(subject, object, amount)?;
+    let result: Vec<(&str, &str, f64)> = [(subject, object, amount)].to_vec();
+    let v: Vec<u8> = rmp_serde::to_vec(&result)?;
+    Ok(v)
+}
+
+fn meritrank_add(
     subject: &str,
     object: &str,
     amount: f64,
